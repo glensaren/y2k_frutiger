4e3e4ec840cd2bbcf38c389ea43dd50e4bdbf304
Glen <imaf7024@gmail.com>
2025-07-18 07:03:59 +0300

    image pashalko

---
+++ notebook.js
@@ -0,0 +1,6 @@
+const pageImage = document.querySelector('.page-image')
+const dialogWindow = document.querySelector('.dialog-window')
+
+pageImage.addEventListener('click' , ()=> {
+    dialogWindow.showModal()
+})
